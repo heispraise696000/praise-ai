1d233e1afc836952e43f27262cd3f980e7907025
heispraise696000 <inemopraise147@gmail.com>
2025-11-03 17:15:28 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+app.use(cors());
 const bodyParser = require('body-parser');
 const axios = require('axios');
 require('dotenv').config();
